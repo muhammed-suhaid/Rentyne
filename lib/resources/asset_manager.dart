0e5ab2e6e19baaa3c72268eade8964c973463518
Muhammed suhaid <muhammedsuhaid3@gmail.com>
2025-04-25 22:07:23 +0530

    added resources

---
+++ lib/resources/asset_manager.dart
@@ -0,0 +1,10 @@
+const String imagePath = 'assets/images';
+
+class ImageAssets {
+  //************  GET STARTED IMAGE *************************
+
+  static const String getStarted1 = '$imagePath/get_started1.jpg';
+  static const String getStarted2 = '$imagePath/get_started2.jpg';
+  static const String getStarted3 = '$imagePath/get_started3.jpg';
+  
+}
